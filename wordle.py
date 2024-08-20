1b12cba8c5cb6228e330cecdab8e981dc41c16ab
stergoat trotter <stergoat25@gmail.com>
2024-08-20 16:40:06 +0000

    Completed Task A

--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 # TASK A: Define a variable 'word' that holds the correct word for the wordle game
+word = "START"
 
 
 # TASK B: Define a function 'makeAGuess()' that passes in a users guess as a parameter
